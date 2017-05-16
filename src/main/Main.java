94901751ca14228d5b574ba73f36d054d813e82b
asampley <sampley@ualberta.ca>
2017-05-16 14:14:08 -0600

    Caused program to exit if insufficient arguments are supplied

--- src/main/Main.java
+++ src/main/Main.java
@@ -33,6 +33,7 @@ public class Main {
 	public static void main(String[] args) {
 		if (args.length < 1) {
 			System.out.println("Usage: <recipe directory>");
+			System.exit(1);
 		}
 		
 		System.out.println("Welcome to the Factorio Ratio Assistant");
